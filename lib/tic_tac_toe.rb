56551b5d26987b99fc298d545f5aa988fa98e792
Jesse Markowitz <jess.markowitz@gmail.com>
2020-06-29 19:13:08 +0000

    Automatically backed up by Learn

---
+++ lib/tic_tac_toe.rb
@@ -0,0 +1,82 @@
+class TicTacToe
+  # Initialize the game board w/new instance
+  def initialize(board = nil)
+    @board = board || Array.new(9, " ")
+  end
+
+  # WIN_COMBINATIONS constant; 2-level array of all possible win combinations
+  WIN_COMBINATIONS = [
+    [0,1,2],
+    [3,4,5],
+    [6,7,8],
+    [0,3,6],
+    [1,4,7],
+    [2,5,8],
+    [0,4,8],
+    [2,4,6]
+  ]
+
+  # Display the board, whatever its contents
+  def  display_board
+    puts " #{@board[0]} | #{@board[1]} | #{@board[2]} "
+    puts "-----------"
+    puts " #{@board[3]} | #{@board[4]} | #{@board[5]} "
+    puts "-----------"
+    puts " #{@board[6]} | #{@board[7]} | #{@board[8]} "
+  end
+
+  # Turn the user's move input into an array index
+  def input_to_index(input)
+    input.to_i - 1
+  end
+
+  # Allow a player to inpupt their move into the board
+  def move(index, token = "X")
+    @board[index] = token
+  end
+
+  # Determine if a specific board position is taken; return "true" if so
+  def position_taken?(index)
+    @board[index] == "X" || @board[index] == "O"
+  end
+
+  # Determine if a user's move is valid; return "true" if so
+  def valid_move?(index)
+    if !position_taken?(index) && index.between?(0, 8)
+      return true
+    else
+      return false
+    end
+  end
+
+  # Count the number of turns that have been taken
+  def turn_count
+    turns = 0
+    @board.each do |position|
+      if position == "X" || position == "O"
+        turns += 1
+      end
+    end
+    return turns
+  end
+
+  # Determine whose turn it is currently
+  def current_player
+    turn_count.even? ? "X" : "O"
+  end
+
+  # A complete turn
+  def turn
+    token = current_player
+    puts "Player #{token}, please enter 1-9:"
+    input = gets.strip
+    index = input_to_index(input)
+    if valid_move?(index) == false
+      puts "Sorry, invalid move."
+      turn
+    else
+      move(index, token)
+    end
+    display_board
+  end
+end
